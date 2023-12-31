b9a19e215edfc77973dee005e4c475adfe253fa1
Deepak <deekay9357@gmail.com>
2023-12-31 22:25:52 +0530

    @1.0.5 MainPageInitial

--- app/src/main/java/com/vyarth/ellipsify/activities/MainActivity.kt
+++ app/src/main/java/com/vyarth/ellipsify/activities/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.vyarth.ellipsify.activities
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
 import com.vyarth.ellipsify.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // Set the status bar text color to dark
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        val profileUser: CircleImageView = findViewById(R.id.user_avatar)
+        profileUser.setOnClickListener {
+            // Launch the sign in screen.
+            startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+        }
     }
 }
